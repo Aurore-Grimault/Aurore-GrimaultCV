49e69ccf18f1258f590be29288b22f44f294965f
Aurore-Grimault <auroregrimault@hotmail.fr>
2022-04-13 13:49:06 +0200

    commit 1

---
+++ main.js
@@ -0,0 +1,9 @@
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // dark mode
+}
+
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
